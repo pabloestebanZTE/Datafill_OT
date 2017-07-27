61c7b85730e06b1d568b6dfffcfbe4d0f7669f7b
unknown <bredybuitrago@gmail.com>
2017-07-27 16:57:59 -0500

    se añadieron las vistas de login y principal

---
+++ application/views/index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+       <title>Principal</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <!-- Navigation -->
+     <header>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="logo"><img id="logo" src="assets/img/logo2.png" /></a>
+            </div>
+            <!-- Collect the nav links for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="cam"><a href="#home">HOME</a>
+                    </li>
+                    <li class="cam"><a href="#services">SERVICIOS</a>
+                    <ul>
+                        <li><a href="">AGENDAR ACTIVIDAD</a></li>
+                        <li><a href="">VER ACTIVIDAD</a></li>
+                        <li><a href="">ASIGNAR ACTIVIDAD</a></li>
+                    </ul>
+                    </li>
+                    <li class="cam"><a href="#price-sec">PRICING</a>
+                    </li>
+                     <li class="cam"><a href="#contact-sec">CONTACT</a>
+                    </li>   
+                </ul>
+            </div>
+        </div>
+    </nav>
+     </header>
+    <!--End Navigation -->
+    <!--Header section  -->
+    <div id="home">
+    <div class="container" >
+        <div class="row ">
+            <div class="col-md-9 col-sm-9">
+    <div><a><img class="logoz" id="logoz" src="assets/img/logoz.png" /></a></div>
+                <h1 class="head-main">ZTE</h1>
+                <span class="head-sub-main">Tomorrow never waits</span>
+                <div class="head-last"><!--texto aca--> </div>
+            </div>
+        </div>
+    </div>
+          </div>
+    <!--End Header section  -->
+    <!--Services Section-->
+    <section  id="services">
+        <div class="container">          
+            <div class="row text-center">
+                <div class="col-md-8 col-md-offset-2">
+                    <h2>Our Projects</h2>                   
+                </div>
+            </div>
+            <div class="row text-center space-pad">
+                <div class="col-md-3 col-sm-3">
+                    <div >
+                        <i class="fa fa-camera fa-4x main-color"></i>
+                        <h3>Responsive </h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <div >
+                        <i class="fa fa-briefcase fa-4x main-color"></i>
+                        <h3>100% Free </h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <div >
+                        <i class="fa fa-desktop fa-4x main-color"></i>
+                        <h3>Clean Code </h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <div>
+                        <i class="fa fa-folder fa-4x main-color"></i>
+                        <h3>Read to Use </h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                        </p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    </section>
+     <!--End Services Section-->
+    <!--parallax one-->
+      <section  id="Parallax-one">
+    <div class="container">
+            <div class="row text-center">
+                <div class="col-md-8 col-md-offset-2 ">
+                    <h2><i class="fa fa-desktop fa-3x"></i>&nbsp;Just Space </h2>
+                    <h4><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                        </strong>
+                    </h4>
+                </div>
+            </div>
+        </div>
+          </section>
+    <section  id="contact-sec">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">                
+                    <div id="social-icon">
+                          <strong> Visita:</strong> ztemobilecolombia.com
+                        <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-linkedin fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+                        <a href="#"><i class="fa fa-pinterest fa-2x"></i></a>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </section>
+    <!--End Contact Section -->
+    <!--footer Section -->
+    <div class="for-full-back " id="footer">
+        ZTE Designed By ZTE Colombia | All Right Reserved         
+    </div>
+    <!--End footer Section -->
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY  -->
+    <script src="assets/plugins/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP CORE SCRIPT   -->
+    <script src="assets/plugins/bootstrap.js"></script>
+    <!-- PARALLAX SCRIPT   -->
+    <script src="assets/plugins/4jquery.parallax-1.1.3.js"></script>
+    <!-- CUSTOM SCRIPT   -->
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
---
+++ application/views/login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>ZOLID LOGIN</title>
+  <script src="https://s.codepen.io/assets/libs/modernizr.js" type="text/javascript"></script>
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  <link rel="stylesheet" href="/ZolicZTE/assets/css/stylelogin.css">
+    
+    
+  <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>-->
+
+</head>
+
+<body>
+  <div id="warp">
+  <H2></H2>
+  <form action="" id="formu">
+    <div class="admin">
+      <div class="rota">
+        <h1>ZOLID</h1>
+        <input id="username" type="text" name="name" value="" placeholder="Username" /><br />
+        <input id="password" type="password" name="pass" value="" placeholder="Password" />
+         <td><input type="text" name="proyecto" list="project" placeholder="seleccione su proyecto" ></td>
+            <datalist id="project">
+              <option value="Fonade" />
+              <option value="Claro" />
+              <option value="Telmex VIP" />
+              <option value="Facturacion" />
+              <option value="project x" />
+              <option value="project x" />
+              <option value="project x" />
+            </datalist>
+            </tr>
+      </div>
+    </div>
+    <div class="cms">
+      <div class="roti">
+        <h1>ZTE</h1>
+        <button id="valid" type="button" name="valid">login</button><br />
+        <p><a href="#">ZTE</a> <a>And</a> <a href="#">ZTE Colombia</a></p>
+      </div>
+    </div>
+  </form>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="/ZolicZTE/assets/js/index.js"></script>
+
+</body>
+</html>
